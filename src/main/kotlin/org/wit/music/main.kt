b9bb35cb09e7c39868e1a2742e4cede45cc8a6fe
patrick-on <paddyon99@gmail.com>
2021-10-05 14:40:04 +0100

    Adding Logging Support

---
+++ src/main/kotlin/org/wit/music/main.kt
@@ -0,0 +1,10 @@
+package org.wit.music
+
+import mu.KotlinLogging
+
+private val logger = KotlinLogging.logger {}
+
+fun main(args: Array<String>){
+    logger.info { "Launching Music Console App" }
+    println("Music Kotlin App Version 1.0")
+}
