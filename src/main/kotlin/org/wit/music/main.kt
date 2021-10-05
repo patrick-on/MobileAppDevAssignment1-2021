ee49aa1c8249299eb324b59c9bf91300badc167d
patrick-on <paddyon99@gmail.com>
2021-10-05 14:48:13 +0100

    Adding some Menu Support

--- src/main/kotlin/org/wit/music/main.kt
+++ src/main/kotlin/org/wit/music/main.kt
@@ -7,4 +7,37 @@ private val logger = KotlinLogging.logger {}
 fun main(args: Array<String>){
     logger.info { "Launching Music Console App" }
     println("Music Kotlin App Version 1.0")
+
+    var input: Int
+
+    do {
+        input = menu()
+        when(input) {
+            1 -> println("You Chose Add Song")
+            -1 -> println("Exiting App")
+            else -> println("Invalid Option")
+        }
+        println()
+    } while (input != -1)
+    logger.info { "Shutting Down Music Console App" }
+}
+
+fun menu() : Int {
+
+    var option : Int
+    var input: String? = null
+
+    println("Main Menu")
+    println(" 1. Add Song")
+    println(" 2. Update Song")
+    println(" 3. List All Songs")
+    println("-1. Exit")
+    println()
+    print("Enter an integer : ")
+    input = readLine()!!
+    option = if (input.toIntOrNull() != null && !input.isEmpty())
+        input.toInt()
+    else
+        -9
+    return option
 }
